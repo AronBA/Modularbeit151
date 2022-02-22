63edf427627ad5c76cfc9ab1f9a4ea5bec5f0802
AronBA <72823328+AronBA@users.noreply.github.com>
2022-02-22 16:48:30 +0100

    comit suicide

--- js/scripts.js
+++ js/scripts.js
@@ -4,35 +4,3 @@ document.querySelector('.select-field').addEventListener('click',()=>{
 
 });
 
-jQuery(document).ready(function($) {
-    /** ******************************
-     * Simple WYSIWYG
-     ****************************** **/
-    $('#editControls a').click(function(e) {
-        e.preventDefault();
-        switch($(this).data('role')) {
-            case 'h1':
-            case 'h2':
-            case 'h3':
-            case 'p':
-                document.execCommand('formatBlock', false, $(this).data('role'));
-                break;
-            default:
-                document.execCommand($(this).data('role'), false, null);
-                break;
-        }
-
-        var textval = $("#editor").html();
-        $("#editorCopy").val(textval);
-    });
-
-    $("#editor").keyup(function() {
-        var value = $(this).html();
-        $("#editorCopy").val(value);
-    }).keyup();
-
-    $('#checkIt').click(function(e) {
-        e.preventDefault();
-        alert($("#editorCopy").val());
-    });
-});
