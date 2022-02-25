3b4290d5bb78232f24d506989fb878ceb635df1d
GreenWylf <72823328+AronBA@users.noreply.github.com>
2022-02-25 13:01:00 +0100

    Update DB_config.php

--- common/Backend/DB/DB_config.php
+++ common/Backend/DB/DB_config.php
@@ -1,5 +1,5 @@
 <?php
-define('USER', 'root');
-define('PASSWORD', '');
-define('HOST', 'localhost');
-define('DATABASE', 'db_m151_modularbeit');
+define('USER', 'username');
+define('PASSWORD', 'yourpassword');
+define('HOST', 'hostname');
+define('DATABASE', 'databasename');
